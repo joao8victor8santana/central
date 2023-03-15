aa8bbeed641f52f556e5c41da4b520601100a746
joao8victor8santana <1joao2victor3santana@gmail.com>
2023-03-15 10:53:48 -0300

    organizando linhas.

--- conteudo/pag-html.js
+++ conteudo/pag-html.js
@@ -2,7 +2,13 @@ prolin = document.querySelector("p#style")
 prolin.addEventListener("mouseenter", entrarpro)
 prolin.addEventListener("mouseout", sairpro)
 
-function entrarpro() { prolin.innerHTML = "<p>Um projeto mucho loko.</p>" }
 
 
-function sairpro() { prolin.innerHTML = "<p>PeriStyle</p>" }
+
+function entrarpro() {
+  prolin.innerHTML = "<p>Um projeto mucho loko.</p>"
+}
+
+function sairpro() {
+  prolin.innerHTML = "<p>PeriStyle</p>"
+}
